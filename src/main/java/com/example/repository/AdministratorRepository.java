fdf528de4ec997bbc96105b7a52364497337324a
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-12 10:27:55 +0900

    secondcommit

---
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -0,0 +1,37 @@
+package com.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Administrator;
+import com.example.domain.Employee;
+
+@Repository
+public class AdministratorRepository {
+
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+        Employee employee = new Employee();
+        employee.setId(rs.getInt("id"));
+        employee.setName(rs.getString("name"));
+        employee.setImage(rs.getString("image"));
+        employee.setGender(rs.getString("gender"));
+        employee.setHireDate(rs.getDate("hireDate"));
+        employee.setMailAddress(rs.getString("mailAdderess"));
+        employee.setZipCode(rs.getString("zipCode"));
+        employee.setAddress(rs.getString("address"));
+        employee.setTelephone(rs.getString("telephone"));
+        employee.setSalary(rs.getInt("salary"));
+        employee.setCharacteristics(rs.getString("characteristics"));
+        employee.setDependentsCount(rs.getInt("dependentsCount"));
+        return employee;
+    };
+
+    public void insert(Administrator administrator) {
+
+    }
+}
