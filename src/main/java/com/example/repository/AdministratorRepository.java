ffebfbfd00d1a41f94e17dd8f3715418393a2c32
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-12 10:54:23 +0900

    third commit

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -1,12 +1,16 @@
 package com.example.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import com.example.domain.Administrator;
-import com.example.domain.Employee;
 
 @Repository
 public class AdministratorRepository {
@@ -14,24 +18,33 @@ public class AdministratorRepository {
     @Autowired
     private NamedParameterJdbcTemplate template;
 
-    private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
-        Employee employee = new Employee();
-        employee.setId(rs.getInt("id"));
-        employee.setName(rs.getString("name"));
-        employee.setImage(rs.getString("image"));
-        employee.setGender(rs.getString("gender"));
-        employee.setHireDate(rs.getDate("hireDate"));
-        employee.setMailAddress(rs.getString("mailAdderess"));
-        employee.setZipCode(rs.getString("zipCode"));
-        employee.setAddress(rs.getString("address"));
-        employee.setTelephone(rs.getString("telephone"));
-        employee.setSalary(rs.getInt("salary"));
-        employee.setCharacteristics(rs.getString("characteristics"));
-        employee.setDependentsCount(rs.getInt("dependentsCount"));
-        return employee;
+    private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+        Administrator administrator = new Administrator();
+        administrator.setId(rs.getInt("id"));
+        administrator.setName(rs.getString("name"));
+        administrator.setMailAddress(rs.getString("mailAdderss"));
+        administrator.setPassword(rs.getString("password"));
+        return administrator;
     };
 
+    /** 管理者情報を挿⼊ */
     public void insert(Administrator administrator) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
 
+        if (administrator.getId() == null) {
+            String insertSql = "INSERT INTO administrators(id, name, mailAddress, password) VALUES(:id, :name, :mailAddress, :password)";;
+            template.update(insertSql, param);
+        }
+    }
+
+    /** メールアドレスとパスワードから管理者情報を取得する(1件も存在しない場合は null を返す※) */
+    public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
+        String sql = "SELECT id, name, mailAddress, password FROM administrators WHERE mailAddress = :mailAddress AND password = :password;";
+        SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
+        List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+        if (administratorList.size() == 0) {
+            return null;
+        }
+        return administratorList.get(0);
     }
 }
