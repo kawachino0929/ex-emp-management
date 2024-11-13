5e31b7140d9bb3b2f306f17ccbbfdf9f4065e2c7
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-13 14:51:55 +0900

    Administratorのinsertメソッドにバリデーション作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +36,15 @@ public class AdministratorController {
 
     /**
      * 
+     * バリデーション用のrequestスコープ作成
+     */
+    @ModelAttribute
+    public InsertAdministratorForm setUpForm() {
+        return new InsertAdministratorForm();
+    }
+
+    /**
+     * 
      * 管理者用のサービスクラスのメソッドが使えるようにする
      */
     @Autowired
@@ -56,7 +68,10 @@ public class AdministratorController {
      * @param InsertAdministratorForm フォームオブジェクトをリクエストスコープに⾃動的に格納
      */
     @PostMapping("/insert")
-    public String insert(InsertAdministratorForm form) {
+    public String insert(@Validated InsertAdministratorForm form, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return toInsert(form);
+        }
         Administrator administrator = new Administrator();
         BeanUtils.copyProperties(form, administrator);
         administratorService.insert(administrator);
