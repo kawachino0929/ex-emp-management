de2876f34bda35af7c0a6aef4556b871271f5481
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-13 13:28:57 +0900

    EmployeeControllerでupdateメソッドを作成しました

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Employee;
@@ -15,25 +16,27 @@ import com.example.service.EmployeeService;
  * 
  * 従業員用コントローラー
  * 従業員一覧や従業員を閲覧
+ * 
  * @author 川内野孝樹
  */
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
-    /**
-     * 
-     * 従業員用のサービスクラスのメソッドが使えるようにする
-     */
+  /**
+   * 
+   * 従業員用のサービスクラスのメソッドが使えるようにする
+   */
   @Autowired
   private EmployeeService employeeService;
 
-    /**
-     * 
-     * 従業員一覧を出力
-     * @return 従業員一覧画面へ遷移
-     * @param Model リクエストスコープに格納できるようにする
-     */
+  /**
+   * 
+   * 従業員一覧を出力
+   * 
+   * @return 従業員一覧画面へ遷移
+   * @param Model リクエストスコープに格納できるようにする
+   */
   @GetMapping("/showList")
   public String showList(Model model) {
     List<Employee> employeeList = employeeService.showList();
@@ -41,18 +44,33 @@ public class EmployeeController {
     return "employee/list";
   }
 
-    /**
-     * 
-     * 従業員を出力
-     * @return 従業員詳細画面へ遷移
-     * @param id 見たい従業員IDを選択
-     * @param Model リクエストスコープに格納できるようにする
-     * @param UpdateEmployeeForm フォームオブジェクトをリクエストスコープに⾃動的に格納
-     */
+  /**
+   * 
+   * 従業員を出力
+   * 
+   * @return 従業員詳細画面へ遷移
+   * @param id                 見たい従業員IDを選択
+   * @param Model              リクエストスコープに格納できるようにする
+   * @param UpdateEmployeeForm フォームオブジェクトをリクエストスコープに⾃動的に格納
+   */
   @GetMapping("/showDetail")
   public String showDetail(String id, Model model, UpdateEmployeeForm form) {
     Employee employee = employeeService.showDetail(Integer.parseInt(id));
     model.addAttribute("employee", employee);
     return "employee/detail";
+  }
+
+  /**
+   * 
+   * 従業員情報を更新
+   * 
+   * @return /employee/showListへリダイレクト
+   * @param UpdateEmployeeForm フォームオブジェクトをリクエストスコープに⾃動的に格納
+   */
+  @PostMapping("/update")
+  public String update(UpdateEmployeeForm form) {
+    Employee employee = employeeService.showDetail(Integer.parseInt(form.getId()));
+    employeeService.update(employee);
+    return "redirect:/employee/showList"; 
   }
 }
