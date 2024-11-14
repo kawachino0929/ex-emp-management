96ca68b6aa7c00f1bbda4d1b3e692ca33ecf768e
k.kawachino <koki.kawachino@rakus-partners.co.jp>
2024-11-14 16:11:46 +0900

    ログアウト関連修正しました

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -115,6 +114,6 @@ public class AdministratorController {
     @GetMapping("/logout")
     public String logout(LoginForm form) {
         session.invalidate();
-        return "redirect:/";
+        return "administrator/login";
     }
 }
--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -8,12 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Employee;
-import com.example.form.InsertAdministratorForm;
 import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
 
